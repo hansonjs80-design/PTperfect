4b1b26f2899600b03ffd46629ba36e3544c87628
hansonjs80-design <hanson.js80@hansol80.page>
2026-03-12 23:17:27 +0900

    Refine treatment-name checks in patient log row

--- components/LandscapeCells.tsx
+++ components/LandscapeCells.tsx
@@ -64,7 +64,7 @@ export const LandscapeEmptyCell: React.FC = memo(() => {
   };
 
   return (
-    <div className="w-full h-full flex gap-1.5 lg:gap-2 p-1 lg:p-0 min-h-[120px] sm:min-h-full">
+    <div className="w-full h-full md:h-[95%] md:self-start xl:h-[77%] flex gap-1.5 lg:gap-2 p-1 lg:p-0 min-h-[120px] sm:min-h-full xl:min-h-0">
       {/* Undo / Redo Group */}
       <div className="flex-1 flex flex-col gap-1.5 h-full">
         {/* Undo Button */}
